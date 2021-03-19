1e28eabc41ff6a5381a98279e98e89a2550c7c14
Mohamed-Nady13 <80857714+Mohamed-Nady13@users.noreply.github.com>
2021-03-20 01:16:14 +0200

    Add Navigation Feature

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -11,19 +11,18 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
-
+ */
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -31,27 +30,55 @@
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 
+const sections = document.querySelectorAll('section');
+
+const frag = document.createDocumentFragment();
+
+sections.forEach(element => {
+    const newLi = document.createElement(`li`);
+
+    const anchor = document.createElement('a');
+    anchor.innerHTML = element.getAttribute("data-nav");
+    anchor.setAttribute("href", "#" + element.id);
+    anchor.className = "menu__link";
+
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        scrollToElement(element.id);
+    });
+
+    newLi.appendChild(anchor);
+    frag.appendChild(newLi);
+    // myNav.appendChild(newLi);
+});
+
+const myNav = document.querySelector("#navbar__list");
+myNav.appendChild(frag);
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToElement(elementId) {
+    const element = document.querySelector("#" + elementId);
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    });
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
